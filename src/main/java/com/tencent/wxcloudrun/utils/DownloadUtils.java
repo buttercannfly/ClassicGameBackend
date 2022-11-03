d03ad2daf0c20423df0f74b007eb3a35b4d48872
zwk <1710085142@qq.com>
2022-11-04 00:42:35 +0800

    ResourcePatternResolver

--- src/main/java/com/tencent/wxcloudrun/utils/DownloadUtils.java
+++ src/main/java/com/tencent/wxcloudrun/utils/DownloadUtils.java
@@ -1,25 +1,34 @@
 package com.tencent.wxcloudrun.utils;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.ResourceUtils;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class DownloadUtils {
     public static void download(String filename, HttpServletResponse res) throws IOException {
         // 发送给客户端的数据
         // 读取filename
-        ClassPathResource classPathResource = new ClassPathResource("template/"+filename);
-        long length = classPathResource.getFile().length();
-        res.addHeader("Content-Length",String.valueOf(length));
+//        File file = ResourceUtils.getFile("classpath:"+filename);
+//        InputStream in = new FileInputStream(file);
+
+//        ClassPathResource classPathResource = new ClassPathResource(filename);
+
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(filename);
+        Resource resource = resources[0];
+        InputStream in = resource.getInputStream();
+
+//        long length = resource.getFile().length();
+//        res.addHeader("Content-Length",String.valueOf(length));
         OutputStream outputStream = res.getOutputStream();
         byte[] buff = new byte[1024];
         BufferedInputStream bis = null;
-        InputStream inputStream =classPathResource.getInputStream();
-        bis = new BufferedInputStream(inputStream);
+        bis = new BufferedInputStream(in);
         int i = bis.read(buff);
         while (i != -1) {
             outputStream.write(buff, 0, buff.length);
